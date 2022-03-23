f9a9da3a47789331150e24bb8ce340a7d069a1af
mahabur <mahaburcse@gmail.com>
2022-03-23 18:57:55 +0600

    first program

---
+++ add-two-number.js
@@ -0,0 +1,11 @@
+// first way
+const num1 = 5
+const num2 = 7
+console.log(`Sum of two number is ${num1 + num2}`)
+
+// second way
+// const x = parseInt(prompt("Enter first number: "))
+// const y = parseInt(prompt("Enter second number: "))
+// const sum = (x+y)
+// console.log("Result of sum is: ",sum)
+
