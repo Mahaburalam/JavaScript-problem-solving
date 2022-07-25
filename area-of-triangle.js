6baee05db2dd0fc4b64e2747246c8e4f0f61c1b0
mahabur <mahaburcse@gmail.com>
2022-07-25 14:43:53 +0600

    few more solution added

---
+++ area-of-triangle.js
@@ -0,0 +1,5 @@
+let baseValue = prompt("Enter first value: ");
+let baseHeight = prompt("Enter second value: ");
+let totalArea = .5 * (baseValue * baseHeight);
+
+console.log(`Area of triangle result is: ${totalArea}`);
---
+++ square-root.js
@@ -0,0 +1,6 @@
+let num = 9;
+let num1 = 19;
+let num2 = -5;
+let result = Math.sqrt(num2);
+
+console.log(`The output is: ${result}`);
