2c8fa07968abfa86d2f0da8f58f321d9a727cbb0
Mahaburalam <mahaburcse@gmail.com>
2024-03-21 11:59:23 +0600

    Boolean to String Conversion

---
+++ Boolean to String Conversion.js
@@ -0,0 +1,21 @@
+// Create a function that takes a boolean variable flag and returns it as a string.
+// Examples
+/*
+    boolToString(true) ➞ "true"
+    boolToString(false) ➞ "false"    
+*/
+
+function toString(flag)
+{
+    const boolToString = flag.toString();
+    return boolToString;
+}
+console.log(toString(false), typeof(toString(false)));
+
+
+
+
+
+
+
+
