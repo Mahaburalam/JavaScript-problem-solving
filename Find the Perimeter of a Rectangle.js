ed4fd7a3c55069e4f1b26326369559726d6cbdaa
Mahaburalam <mahaburcse@gmail.com>
2024-03-21 16:16:43 +0600

    Find the Perimeter of a Rectangle

---
+++ Find the Perimeter of a Rectangle.js
@@ -0,0 +1,8 @@
+// Find the Perimeter of a Rectangle
+// formula : 2(length + width)
+
+function perimeterRectangle(length, width)
+{
+    return 2 * (length + width);
+}
+console.log(perimeterRectangle(2, 9));
---
+++ Return the Next Number from the Integer Passed.js
@@ -0,0 +1,7 @@
+// Return the Next Number from the Integer Passed
+function nextIntegerNumber(number)
+{
+    return (number + 1);
+}
+console.log(nextIntegerNumber(0));
+console.log(nextIntegerNumber(-4));
